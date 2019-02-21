300906d19e9b42ca0ad41ab5baca64f048e7c697
Jack <jack.fw.holtby@gmail.com>
2019-02-21 22:50:17 +0800

    Fixed #2 in beginning comment.

--- logsAnalysis.py
+++ logsAnalysis.py
@@ -2,7 +2,7 @@
 # Author: Jack Holtby
 # Purpose: Connect to the news postgresql database and return:
 # 1. The three most popular articles of all time in the database.
-# 2. A list of the authors listed in order of how prolific they are (prolificacy?).
+# 2. A list of the authors listed in order of popularity based on article views.
 # 3. A list of the days on which more than 1% of requests lead to errors.
 #
 # These will be output in text. No arguments. No input.
